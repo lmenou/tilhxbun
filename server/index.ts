ae5d949b479076760e38a1231578ea58ff0e1a69
lmenou <menl94629@gmail.com>
2023-09-18 13:21:16 +0200

    feat: format files

--- server/index.ts
+++ server/index.ts
@@ -1,31 +1,31 @@
-import { serve, file } from "bun";
-import path from "path";
+import { serve, file } from 'bun';
+import path from 'path';
 
-const BASE_PATH: string = "../client";
+const BASE_PATH: string = '../client';
 const CLIENT_DIR: Record<string, string> = {
-  ".html": BASE_PATH,
-  ".css": BASE_PATH,
-  ".js": path.join(BASE_PATH, "javascript"),
-  ".ico": path.join(BASE_PATH, "assets"),
+    '.html': BASE_PATH,
+    '.css': BASE_PATH,
+    '.js': path.join(BASE_PATH, 'javascript'),
+    '.ico': path.join(BASE_PATH, 'assets'),
 };
 
 const file_serving = (fileName: string) => {
-  let dotoffset: number = fileName.toString().lastIndexOf(".");
-  const extension = fileName.slice(dotoffset);
-  return path.join(CLIENT_DIR[extension] + fileName);
+    let dotoffset: number = fileName.toString().lastIndexOf('.');
+    const extension = fileName.slice(dotoffset);
+    return path.join(CLIENT_DIR[extension] + fileName);
 };
 
 serve({
-  async fetch(req) {
-    let fileName = new URL(req.url).pathname;
-    if (fileName == "/") {
-      fileName = fileName + "index.html";
-    }
-    const relativeFileName: string = file_serving(fileName);
-    console.log(relativeFileName);
-    return new Response(file(relativeFileName));
-  },
-  error() {
-    return new Response(null, { status: 404 });
-  },
+    async fetch(req) {
+        let fileName = new URL(req.url).pathname;
+        if (fileName == '/') {
+            fileName = fileName + 'index.html';
+        }
+        const relativeFileName: string = file_serving(fileName);
+        console.log(relativeFileName);
+        return new Response(file(relativeFileName));
+    },
+    error() {
+        return new Response(null, { status: 404 });
+    },
 });
