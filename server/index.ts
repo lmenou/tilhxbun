9429d59b26e5cb7514e9587da473d9be282c9e4f
lmenou <menl94629@gmail.com>
2023-09-18 11:56:26 +0200

    fix(server): handle all types of files

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,31 @@
-import { serve, file } from 'bun';
-import path from 'path';
+import { serve, file } from "bun";
+import path from "path";
+
+const BASE_PATH: string = "../client";
+const CLIENT_DIR: Record<string, string> = {
+  ".html": BASE_PATH,
+  ".css": BASE_PATH,
+  ".js": path.join(BASE_PATH, "javascript"),
+  ".ico": path.join(BASE_PATH, "assets"),
+};
+
+const file_serving = (fileName: string) => {
+  let dotoffset: number = fileName.toString().lastIndexOf(".");
+  const extension = fileName.slice(dotoffset);
+  return path.join(CLIENT_DIR[extension] + fileName);
+};
 
 serve({
-  fetch(req) {
-      if(req.url.endsWith("/") == true) {
-          const header = new Headers();
-          header.append("Content-Type", "text/html");
-          const dest = path.join(import.meta.dir, '/', '..', 'client', 'index.html');
-          const resp = new Response(file(dest), { headers: header });
-          return resp;
-      } else {
-          return new Response('404!')
-      }
+  async fetch(req) {
+    let fileName = new URL(req.url).pathname;
+    if (fileName == "/") {
+      fileName = fileName + "index.html";
+    }
+    const relativeFileName: string = file_serving(fileName);
+    console.log(relativeFileName);
+    return new Response(file(relativeFileName));
+  },
+  error() {
+    return new Response(null, { status: 404 });
   },
 });
