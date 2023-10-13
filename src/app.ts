0352f7a6f5c3c0441e7e4c5ec933d98e780dace9
lmenou <menl94629@gmail.com>
2023-10-13 09:02:40 +0200

    feat(server): parse xml response

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Elysia } from 'elysia';
 import { staticPlugin } from '@elysiajs/static';
 import { Field } from './utils';
 import { query_prefix } from './utils';
+import { XMLParser } from 'fast-xml-parser';
 
 const QUERY_URL = new URL('http://export.arxiv.org/api/query');
 
@@ -15,15 +16,17 @@ export const app = new Elysia().use(staticPlugin());
 app.get('/', (context) => (context.set.redirect = '/public/index.html'));
 
 app.post('/arxiv_query', (context) => {
-    const query = QUERY_URL;
-    const res = build(context.body as Field);
+    let query = QUERY_URL;
+    let res = build(context.body as Field);
     query.searchParams.append('search_query', res);
     query.searchParams.append('start', '0');
     query.searchParams.append('max_result', '10');
     fetch(query)
-        .then((response) => response.blob())
+        .then((response) => response.text())
         .then((data) => {
-            console.log(data);
+            const parser = new XMLParser();
+            let jObj = parser.parse(data);
+            console.log(jObj);
         });
     return `<p>${(context.body as Field).author}</p>`;
 });
